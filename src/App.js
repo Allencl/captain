c900e4bce1c623eb92c2c3fb1959fd0c9cf70c2b
Allencl <Allen__cc@163.com>
2021-12-15 14:23:32 +0800

    11

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'antd/dist/antd.css'; 
+import LayoutPage from './Layout.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <LayoutPage />
+    </div>
+  );
+}
+
+export default App;
