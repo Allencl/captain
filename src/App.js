e279017fdce5101c4b532eae4b533391170e6610
Allencl <Allen__cc@163.com>
2021-12-15 16:12:01 +0800

    22

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <h1><Button type="primary">Primary Button</Button></h1>
+    </div>
+  );
+}
+
+export default App;
