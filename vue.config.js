f94f3a480bfa5591ef27fa71d012eb50d16cd440
Allencl <Allen__cc@163.com>
2024-12-26 16:09:06 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -27,7 +27,22 @@ module.exports = defineConfig({
   },
   pluginOptions: {
     electronBuilder: {
-      customFileProtocol: "./"
+      customFileProtocol: "./",
+      builderOptions:{
+        // productName:"SINOBOOM112",    // 项目名（exe 文件的前缀名)
+        nsis: {
+          // oneClick: false, // 是否一键安装
+          // allowToChangeInstallationDirectory: true, //是否允许修改安装目录
+          uninstallerIcon: "./public/logo.ico",//卸载时图标
+        },
+        win:{
+          // version: '1.2.3',      // 版本号
+          // artifactName: '${productName}-${platform}-${arch}-11.22.55.${ext}',
+          icon: './public/logo.ico',   // 安装时的图标
+          target: "nsis"
+
+        }
+      },
     }
   }
 })
