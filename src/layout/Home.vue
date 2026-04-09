4f030709fcb9b9d70af0bc40a8822820641537a6
Allencl <Allen__cc@163.com>
2026-04-09 09:14:44 +0800

    1

--- src/layout/Home.vue
+++ src/layout/Home.vue
@@ -398,7 +398,7 @@
           
           // if( [4,9,14,19,24,29,34,39,44,49,54,59].includes(minutes) ){
           // if( [14,29,44,59].includes(minutes) ){
-          if( [9,19,29,39,49,59].includes(minutes) ){
+          if( [14,29,44,59].includes(minutes) ){
 
             if( (_hours >=8) && (_getDay!=0) && (_getDay!=6) ){
 
@@ -510,12 +510,12 @@
             this.chipList=  this.chipList.concat([_json])
             
 
-            this.$nextTick(()=>{
-              setTimeout(()=>{
-                this.valueAudio=[]
-                this.valueNumberText=5
-              },300)
-            })
+            // this.$nextTick(()=>{
+            //   setTimeout(()=>{
+            //     this.valueAudio=[]
+            //     this.valueNumberText=5
+            //   },300)
+            // })
 
 
             this.$nextTick(()=>{
